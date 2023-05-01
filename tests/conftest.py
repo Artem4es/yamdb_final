52b0e53a9ccb664c1bbf929847fd98dfcbab179a
artem4es <dragster1001@yandex.ru>
2023-05-01 21:06:14 +0300

    initial

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,5 @@ from os.path import abspath, dirname, join
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 infra_dir_path = join(root_dir, 'infra')
-print('Путь к infra', infra_dir_path, sep='\n')
 
 pytest_plugins = []
